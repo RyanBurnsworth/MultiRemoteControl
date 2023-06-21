88a5fcb704d3e0440f0abd4ec92fac8041d9ab73
Ryan Burnsworth <ryanburnsworth@gmail.com>
2023-06-21 19:47:36 -0400

    setup Dagger-Hilt

---
+++ app/src/main/java/com/burnsworthrobotics/multiremotecontrol/MultiRemoteControlApplication.kt
@@ -0,0 +1,7 @@
+package com.burnsworthrobotics.multiremotecontrol
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MultiRemoteControlApplication: Application() { }
